76439cdc8d4956c41726d6cf4e3ad3baeec4a470
HengYuZ <zhanghengyu@yaochufa.com>
2016-09-07 11:44:06 +0800

    更新了README.md。描述了如何使用composer安装Dosql

--- README.md
+++ README.md
@@ -1,3 +1,31 @@
 #Dosql
 ##简介
 ##Dosql是一个简单的mysql操作数据库封装类。
+
+##使用
+  新建一个composer.json文件
+  ```
+      {
+        "require":{
+            "php":">=5.3.0",
+            "hengyuz/dosql":"dev-master"
+        },
+        "minimum-stability":"stable"
+      }
+  ```
+  然后保存composer.json文件后，使用composer install 命令进行安装。
+
+  然后使用命令: ```cd vendor/hengyu/dosql```;
+
+  简单使用可以参照show.php。
+##更多用法：
+  Dosql不使用组合条件：
+  ```
+  1. $dosql->select('user','','');
+  2. $dosql->select('user','',['id='=>9]);
+  ```
+  Dosql支持组合条件使用：
+  ```
+  1. $dosql->select('tb_name',['name','id'],['AND'=>[['id='=>9],['name'=>'hengyu']);
+  2. $dosql->select('tb_name',['name','id'],['AND'=>['OR'=>[['id='=>9],['name='=>'hengyu']],'OR'=>[['id='=>5],['name='=>'zhu']]]];
+  ```
---
+++ show.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: zhanghengyu
+ * Date: 2016/9/7
+ * Time: 11:07
+ */
+require 'vendor/autoload.php';
+use HengYuZ\src as mydb;
+
+$dosql = new mydb\Dosql(['localhost' => 'localhost', 'username' => 'root', '', 'db_name' => 'test']);
+
+$res = $dosql->select('users', '', ['name=' => 'zhu']);
+/**
+ * 打印查找后的结果$res数组
+ */
+//foreach ($res as $key => $item) {
+//    $i = 1;
+//    foreach ($item as $key2 => $value) {
+//        if ($i % count($item) == 0)
+//            echo $value . '<br/>';
+//        else
+//            echo $value . ' ';
+//        $i++;
+//    }
+//}
+
+//更新数据
+$dosql->update('users', ['name=' => 'zhu'], ['name=' => 'zhy']);
+//插入数据
+$dosql->insert('users', ['123456', '12346513@qq.com']);
+//删除数据
+$dosql->delete('users', ['OR' => ['name=' => '123456', 'email=' => 'hello.com']]);
